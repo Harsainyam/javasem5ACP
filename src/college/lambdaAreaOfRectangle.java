5cf8e8f66fe7b83377d55f8f9dc5d81ff2c0a8df
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2025-07-07 11:55:27 +0530

    sf

---
+++ src/college/lambdaAreaOfRectangle.java
@@ -0,0 +1,15 @@
+interface RectangleArea {
+    int area(int length, int breadth);
+}
+
+public class lambdaAreaOfRectangle {
+    public static void main(String[] args) {
+
+        // Lambda expression to calculate area of a rectangle
+        RectangleArea area = (length, breadth) -> length * breadth;
+
+        // Test the lambda
+        int result = area.area(10, 5);
+        System.out.println("Area of Rectangle: " + result);
+    }
+}
