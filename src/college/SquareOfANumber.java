d8b48e04284a817151529cc48d9afae613e60a3b
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2025-07-07 11:43:33 +0530

    sf

---
+++ src/college/SquareOfANumber.java
@@ -0,0 +1,16 @@
+package college;
+interface SquareOperation {
+    int square(int x);
+}
+
+public class SquareOfANumber {
+    public static void main(String[] args) {
+
+        // Lambda expression to calculate square
+        SquareOperation square = x -> x * x;
+
+        // Test the lambda
+        int result = square.square(7);
+        System.out.println("Square: " + result);
+    }
+}
