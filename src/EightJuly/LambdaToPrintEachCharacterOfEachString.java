05ce5b4369c4b923155f440f28b3f75218694b23
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2025-07-08 11:25:55 +0530

    Eight July

---
+++ src/EightJuly/LambdaToPrintEachCharacterOfEachString.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+public class LambdaToPrintEachCharacterOfEachString {
+    public static void main(String[] args) {
+        String input = "Hello, World!";
+
+        // Lambda expression to print each character of the string
+        IntStream.range(0, input.length()) // stream of indexes 0 to length-1
+                .forEach(i -> System.out.println(input.charAt(i)));
+    }
+}
