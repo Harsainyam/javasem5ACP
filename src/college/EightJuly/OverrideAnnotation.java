1a2d4a8ae78b0c561abb6198a6920c45c564102e
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2025-07-08 11:51:41 +0530

    Eight July

---
+++ src/college/EightJuly/OverrideAnnotation.java
@@ -0,0 +1,19 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class OverrideAnnotation {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.makeSound();  // Output: Dog barks
+    }
+}
