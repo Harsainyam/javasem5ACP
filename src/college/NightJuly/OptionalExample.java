2add746fd86d33944996c2eedf81b8f03803063e
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2025-07-09 11:23:07 +0530

    nightjuly

---
+++ src/college/NightJuly/OptionalExample.java
@@ -0,0 +1,26 @@
+package college.NightJuly;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+        // Create an Optional with a value
+        Optional<String> optionalName = Optional.of("Jaskaran");
+
+        // Print the value if present
+        optionalName.ifPresent(name -> System.out.println("Name is: " + name));
+
+        // Create an empty Optional
+        Optional<String> emptyOptional = Optional.empty();
+
+        // Use orElse to provide a default value if not present
+        String defaultName = emptyOptional.orElse("No Name Found");
+        System.out.println("Default Name: " + defaultName);
+
+        // Another example using ofNullable
+        String str = null;
+        Optional<String> maybeStr = Optional.ofNullable(str);
+
+        System.out.println("Result: " + maybeStr.orElse("Value was null"));
+    }
+}
